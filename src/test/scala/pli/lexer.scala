e2d82cd6ef904c3cef78991809ef009b47dccdff
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-03 17:14:49 +0200

    Add two more lexer tests.

--- src/test/scala/pli/lexer.scala
+++ src/test/scala/pli/lexer.scala
@@ -5,4 +5,16 @@ class LexerSpec extends Spec {
     val lexer = Lexer.forString("")
     lexer.nextTokenType should be (EndOfFile)
   }
+
+  it should "recognize the 'object' keyword" in {
+    val lexer = Lexer.forString("object")
+    lexer.nextTokenType should be (ObjectKeyword)
+    lexer.readNextToken()
+    lexer.nextTokenType should be (EndOfFile)
+  }
+
+  it should "skip whitespace at the beginning of the input string" in {
+    val lexer = Lexer.forString("   foo")
+    lexer.startColumn should be (4)
+  }
 }
