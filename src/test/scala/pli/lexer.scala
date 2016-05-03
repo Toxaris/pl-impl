826f8c388fef262de47831967125c0af5dfa891c
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-03 20:06:44 +0200

    Add one test for reserved words.
    As an example for how to test that an exception is thrown.

--- src/test/scala/pli/lexer.scala
+++ src/test/scala/pli/lexer.scala
@@ -13,6 +13,13 @@ class LexerSpec extends Spec {
     lexer.nextTokenType should be (EndOfFile)
   }
 
+  it should "reject the reserved word 'for'" in {
+    val lexer = Lexer.forString ("var for = 5")
+    intercept[Error] {
+      lexer.readNextToken()
+    }
+  }
+
   it should "skip whitespace at the beginning of the input string" in {
     val lexer = Lexer.forString("   foo")
     lexer.startColumn should be (4)
