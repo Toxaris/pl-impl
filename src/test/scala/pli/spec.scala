58e470666c59e60027d37f726a5e2e334598cb4c
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-07 22:19:23 +0200

    Add simple 'when' word to test syntax.

--- src/test/scala/pli/spec.scala
+++ src/test/scala/pli/spec.scala
@@ -2,4 +2,8 @@ package pli
 
 import org.scalatest._
 
-abstract class Spec extends FlatSpec with Matchers
+abstract class Spec extends FlatSpec with Matchers with Inside {
+  implicit class WhenWord(val subject: String) {
+    def when(context: String): String = subject + " (when " + context + ")"
+  }
+}
