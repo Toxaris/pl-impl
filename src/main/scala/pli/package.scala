b02fadbf078d6dc90e1e9407b3fa35f8549853df
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-07 18:08:29 +0200

    Document package.

---
+++ src/main/scala/pli/package.scala
@@ -0,0 +1,18 @@
+/** An implementation of a small subset of Scala.
+  *
+  * The code in this package is part of the course material for
+  * the course “Implementation of Programming Languages” at
+  * Tübingen University, Germany.
+  *
+  * Main components:
+  *
+  *   - [[Lexer]] ─ Turns a stream of characters into a stream of tokens
+  *   - [[Parser]] ─ Turns a stream of tokens into an abstract syntax tree
+  *
+  * Main data structures:
+  *
+  *   - [[TokenType]] ─ Types of tokens in the token stream
+  *   - [[ASTNode]] ─ Nodes of the abstract syntax tree
+  */
+package object pli {
+}
