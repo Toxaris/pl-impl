fc4740c4b5165e93106e77cfcdf3eecfb7b9c1d2
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-07 22:32:15 +0200

    Use helper methods for test parses.

--- src/test/scala/pli/parser.scala
+++ src/test/scala/pli/parser.scala
@@ -1,41 +1,53 @@
 package pli
 
 class ParserSpec extends Spec with ASTMatchers {
+  def parser(text: String): Parser =
+    Parser.forString(text.stripMargin)
+
+  def expression(text: String): Expression =
+    parser(text).parseExpression
+
+  def statement(text: String): Statement =
+    parser(text).parseStatement
+
+  def program(text: String): Program =
+    parser(text).parseProgram
+
   "The parser" when "looking for expressions" should
   "accept positive integer literals" in {
-    val result = Parser.forString("123").parseExpression
+    val result = expression("123")
     result should be (a [Literal])
     result should have ('value(123))
   }
 
   it should "accept variable references" in {
-    val result = Parser.forString("hello").parseExpression
+    val result = expression("hello")
     result should be (a [Variable])
     result should have ('name("hello"))
   }
 
   it should "accept addition" in {
-    val result = Parser.forString("1 + 2").parseExpression
+    val result = expression("1 + 2")
     result should be (an [Addition])
   }
 
   it should "accept subtraction" in {
-    val result = Parser.forString("1 - 2").parseExpression
+    val result = expression("1 - 2")
     result should be (a [Subtraction])
   }
 
   it should "accept multiplication" in {
-    val result = Parser.forString("1 * 2").parseExpression
+    val result = expression("1 * 2")
     result should be (a [Multiplication])
   }
 
   it should "accept parentheses" in {
-    val result = Parser.forString("(42)").parseExpression
+    val result = expression("(42)")
     result should be (a [Literal])
   }
 
   "Addition " should "be left-associative" in {
-    val result = Parser.forString("1 + 2 + 3").parseExpression
+    val result = expression("1 + 2 + 3")
     result should be (an [Addition])
     inside(result) { case result: Addition =>
       result.lhs should be (an [Addition])
@@ -44,12 +56,12 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "bind as strong as subtraction" in {
-    val result = Parser.forString("1 + 2 - 3").parseExpression
+    val result = expression("1 + 2 - 3")
     result should be (a [Subtraction])
   }
 
   "Subtraction" should "be left-associative" in {
-    val result = Parser.forString("1 - 2 - 3").parseExpression
+    val result = expression("1 - 2 - 3")
     result should be (a [Subtraction])
     inside(result) { case result: Subtraction =>
       result.lhs should be (a [Subtraction])
@@ -58,12 +70,12 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "bind as strong as addition" in {
-    val result = Parser.forString("1 - 2 + 3").parseExpression
+    val result = expression("1 - 2 + 3")
     result should be (an [Addition])
   }
 
   "Multiplication" should "be left-associative" in {
-    val result = Parser.forString("1 * 2 * 3").parseExpression
+    val result = expression("1 * 2 * 3")
     result should be (a [Multiplication])
     inside(result) { case result: Multiplication =>
       result.lhs should be (a [Multiplication])
@@ -72,17 +84,17 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "bind stronger than addition" in {
-    val result = Parser.forString("1 + 2 * 3").parseExpression
+    val result = expression("1 + 2 * 3")
     result should be (an [Addition])
   }
 
   it should "bind stronger than subtraction" in {
-    val result = Parser.forString("1 - 2 * 3").parseExpression
+    val result = expression("1 - 2 * 3")
     result should be (a [Subtraction])
   }
 
   "Parentheses" should "group expressions" in {
-    val result = Parser.forString(" 1 * (2 + 3)").parseExpression
+    val result = expression(" 1 * (2 + 3)")
     result should be (a [Multiplication])
     inside(result) { case result: Multiplication =>
       result.lhs should be (a [Literal])
@@ -92,22 +104,22 @@ class ParserSpec extends Spec with ASTMatchers {
 
   "The parser" when "looking for statements" should
   "accept variable declarations" in {
-    val result = Parser.forString("var answer = 42;").parseStatement
+    val result = statement("var answer = 42;")
     result should be (a [Var])
   }
 
   it should "accept assignments" in {
-    val result = Parser.forString("answer = 42;").parseStatement
+    val result = statement("answer = 42;")
     result should be (an [Assignment])
   }
 
   it should "accept print statements" in {
-    val result = Parser.forString("print(42);").parseStatement
+    val result = statement("print(42);")
     result should be (a [Print])
   }
 
   it should "accept empty blocks" in {
-    val result = Parser.forString("{}").parseStatement
+    val result = statement("{}")
     result should be (a [Block])
     inside(result) { case result: Block =>
       result.body should be (empty)
@@ -115,7 +127,12 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "accept non-empty blocks" in {
-    val result = Parser.forString("{print(1); print(2);}").parseStatement
+    val result = statement("""
+      |{
+      |  print(1);
+      |  print(2);
+      |}
+      |""")
     result should be (a [Block])
     inside(result) { case result: Block =>
       result.body should have (length(2))
@@ -123,17 +140,28 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "accept while loops" in {
-    val result = Parser.forString("while (1) print(2);").parseStatement
+    val result = statement("""
+      |while (1)
+      |  print(2);
+      |""")
     result should be (a [While])
   }
 
   it should "accept if-then-else statements" in {
-    val result = Parser.forString("if (1) print(2); else print(3);").parseStatement
+    val result = statement("""
+      |if (1)
+      |  print(2);
+      |else
+      |  print(3);
+      |""")
     result should be (an [If])
   }
 
   it should "accept if-then statements" in {
-    val result = Parser.forString("if (1) print(2);").parseStatement
+    val result = statement("""
+      |if (1)
+      |  print(2);
+      |""")
     result should be (an [If])
     inside(result) { case result: If =>
       result.elseBranch should be (a [Block])
@@ -144,7 +172,12 @@ class ParserSpec extends Spec with ASTMatchers {
   }
 
   it should "associate dangling else branches to the nearest if" in {
-    val result = Parser.forString("if (1) if (2) {} else print(3);").parseStatement
+    val result = statement("""
+      |if (1)
+      |  if (2) {
+      |  } else
+      |    print(3);
+      |""")
     result should be (an [If])
     inside(result) { case result: If =>
       result.elseBranch should be (a [Block])
@@ -157,12 +190,20 @@ class ParserSpec extends Spec with ASTMatchers {
 
   "The parser" when "looking for a program" should
   "accept programs with empty body" in {
-    val result = Parser.forString("object foo extends bar {}").parseProgram
+    val result = program("""
+      |object foo extends bar {
+      |}
+      |""")
     result.body should be (empty)
   }
 
   it should "accept programs with non-empty body" in {
-    val result = Parser.forString("object foo extends bar {print(1); print(2);}").parseProgram
+    val result = program("""
+      |object foo extends bar {
+      |  print(1);
+      | print(2);
+      |}
+      |""")
     result.body should have (length(2))
 
   }
