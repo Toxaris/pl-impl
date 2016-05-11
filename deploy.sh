71e8fa983e83c1a1845cd610e18495f1c8c17b87
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-11 19:54:38 +0200

    Set up automatic deploy of website changes.
    Not sure whether this will work. Pieced together from various tutorials.

---
+++ deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ev
+
+# clone and checkout out gh-pages into a subdirectory
+git clone --shared --branch gh-pages . gh-pages
+
+# generate index.md
+sed 's|This repository|The [Toxaris/pl-impl](http://github.com/Toxaris/pl-impl) repository on GitHub|' - README.md > gh-pages/index.md <<EOF
+---
+layout: default
+title: Source code for Implementation of Programming Languages course
+---
+EOF
+
+# configure documentation target directory
+cat >> build.sbt <<EOF
+target in Compile in doc := baseDirectory.value / "gh-pages" / "api"
+EOF
+
+# generate documentation
+sbt doc
+
+# figure out hash of HEAD commit
+HEAD=$(git show-ref --head --hash HEAD)
+
+# configure git for commiting website changes
+git config --global user.email "rendel@informatik.uni-tuebingen.de"
+git config --global user.name "Tillmann Rendel (via Travis)"
+
+# commit website changes (and push to main checkout)
+pushd gh-pages
+git add index.md
+git add api
+git commit -m "Update website." -m "Based on $HEAD."
+git push
+popd
+
+# decrypt deployment key
+openssl aes-256-cbc -K $encrypted_0828caebfda7_key -iv $encrypted_0828caebfda7_iv -in deploy-key.enc -out ~/ssh/deploy-key -d
+chmod 600 ~/.ssh/deploy-key
+
+# configure ssh
+cat >> ~/.ssh/config <<EOF
+Host github
+  Hostname github.com
+  IdentityFile ~/.ssh/deploy-key
+  IdentitiesOnly yes
+EOF
+
+# push to github
+git push git@github:Toxaris/pl-impl gh-pages
