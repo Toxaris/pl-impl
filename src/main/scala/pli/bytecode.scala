c28f46e46f52d9ebf81c1ea368d917db7b9c9f32
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2016-05-31 18:13:58 +0200

    Implement builder for bytecode arrays.
    Including an API for appending the various instructions with support for
    calculating jump offsets and backpatching.

---
+++ src/main/scala/pli/bytecode.scala
@@ -0,0 +1,156 @@
+package pli
+
+import collection.mutable;
+
+/** Factory methods for [Bytecode] instances. */
+object Bytecode {
+  def apply() =
+    new Bytecode
+}
+
+/** Builder for bytecode arrays. */
+class Bytecode {
+  /** The buffer for the bytecode array. */
+  var buffer = new Array[Int](1)
+
+  /** The adress of the next element that is added to the bytecode
+    * array. */
+  var adress = 0
+
+  /** Ensures that the buffer is big enough to contain the given adress. */
+  def resize(adress: Int) {
+    var size = buffer.size
+    while (adress >= size) {
+      size *= 2
+    }
+
+    if (size > buffer.size) {
+      val old = buffer
+      buffer = new Array[Int](size)
+      old.copyToArray(buffer)
+    }
+  }
+
+  /** Modifies the entry in the bytecode array at the specified
+    * adress. */
+  def put(adress: Int, value: Int) {
+    resize(adress)
+    buffer(adress) = value
+  }
+
+  /** Patches a jump distance belonging to a jump instruction at
+    * the given adress. */
+  def patch(adress: Int, target: Int) {
+    put(adress + 1, target - adress)
+  }
+
+  /** Appends an integer to the bytecode array and returns the
+    * adress of the newly added integer. */
+  def append(value: Int): Int = {
+    val result = adress
+    put(adress, value)
+    adress += 1
+    result
+  }
+
+  /** Returns the array. */
+  def result =
+    buffer.take(adress)
+
+  /** Appends an `iadd` instruction and returns the adress of the
+    * newly added instructon. */
+  def iadd() =
+    append(Opcode.iadd)
+
+  /** Appends an `isub` instruction and returns the adress of the
+    * newly added instructon. */
+  def isub() =
+    append(Opcode.isub)
+
+  /** Appends an `imul` instruction and returns the adress of the
+    * newly added instructon. */
+  def imul() =
+    append(Opcode.imul)
+
+  /** Appends an `iconst` instruction and returns the adress of the
+    * newly added instructon. */
+  def iconst(value: Int) = {
+    val result = append(Opcode.iconst)
+    append(value)
+    result
+  }
+
+  /** Appends a `goto` instruction and returns the adress of the
+    * newly added instruction.
+    *
+    * @param target the absolute adress of the target of this `goto`
+    * instruction.
+    */
+  def goto(target: Int = 0) = {
+    val base = append(Opcode.goto)
+    append(target - base)
+    base
+  }
+
+  /** Appends an `ifeq` instruction and returns the adress of the
+    * newly added instruction.
+    *
+    * @param target the absolute adress of the target of this `ifeq`
+    * instruction.
+    */
+  def ifeq(target: Int = 0) = {
+    val base = append(Opcode.ifeq)
+    append(target - base)
+    base
+  }
+
+  /** Appends an `ifne` instruction and returns the adress of the
+    * newly added instruction.
+    *
+    * @param target the absolute adress of the target of this `ifne`
+    * instruction.
+    */
+  def ifne(target: Int = 0) = {
+    val base = append(Opcode.ifne)
+    append(target - base)
+    base
+  }
+
+  /** Appends a `print` instruction and returns the adress of the
+    * newly added instruction.
+    */
+  def print() =
+    append(Opcode.print)
+
+  /** Appends an `iload` instruction and returns the adress of the
+    * newly added instruction.
+    */
+  def iload(offset: Int) = {
+    val result = append(Opcode.iload)
+    append(offset)
+    result
+  }
+
+  /** Appends a `istore` instruction and returns the adress of the
+    * newly added instruction.
+    */
+  def istore(offset: Int) = {
+    val result = append(Opcode.istore)
+    append(offset)
+    result
+  }
+
+  /** Appends a `pop` instruction and returns the adress of the new
+    * added instruction. */
+  def pop() =
+    append(Opcode.pop)
+
+  /** Appends an `exit` instruction and returns the adress of the
+    * newly added instruction.
+    */
+  def exit() =
+    append(Opcode.exit)
+
+  override def toString() =
+    Opcode.toString(result)
+}
---
+++ src/test/scala/pli/bytecode.scala
@@ -0,0 +1,385 @@
+package pli
+
+class BytecodeSpec extends Spec {
+  // result
+
+  "Bytecode.result" should "return an empty array if no opcodes where appended" in {
+    val bc = Bytecode()
+    bc.result should have size 0
+  }
+
+  it should "return the bytecode array of all opcodes that were appended" in {
+    val bc = Bytecode()
+    bc.append(27)
+    bc.append(42)
+    bc.append(117)
+    bc.result should be (Array(27, 42, 117))
+  }
+
+  it should "return a snapshot of the bytecode array" in {
+    val bc = Bytecode()
+    bc.append(27)
+    bc.append(42)
+    bc.append(117)
+    val res = bc.result
+    bc.append(123)
+    bc.append(95)
+    res should be (Array(27, 42, 117))
+  }
+
+  // adress
+
+  "Bytecode.adress" when "called on an empty bytecode builder" should
+  "return 0" in {
+    val bc = Bytecode()
+    bc.adress should be (0)
+  }
+
+  // resize
+
+  "Bytecode.resize" should "resize the buffer to contain the given adress" in {
+    val bc = Bytecode()
+    for (adress <- Seq(20, 30, 100, 24039)) {
+      bc.resize(adress)
+      bc.buffer.length should be > adress
+    }
+  }
+
+  it should "keep the old array contents" in  {
+    val bc = Bytecode()
+    bc.resize(2)
+    bc.append(27)
+    bc.append(42)
+    bc.append(117)
+    val oldContents = bc.result
+    bc.resize(100)
+    val newContents = bc.result
+    newContents should be (oldContents)
+  }
+
+  it should "not do anything if the buffer is already big enough" in {
+    val bc = Bytecode()
+    bc.resize(100)
+    val oldBuffer = bc.buffer
+    bc.resize(42)
+    val newBuffer = bc.buffer
+    newBuffer should be (oldBuffer)
+  }
+
+  // iadd
+
+  "Bytecode.iadd" should "append an iadd instruction" in {
+    val bc = Bytecode()
+    bc.iadd()
+    bc.result(0) should be (Opcode.iadd)
+  }
+
+  it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.iadd()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.iadd() should be (3)    
+  }
+
+  // isub
+
+  "Bytecode.isub" should "append an isub instruction" in {
+    val bc = Bytecode()
+    bc.isub()
+    bc.result(0) should be (Opcode.isub)
+  }
+
+  it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.isub()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.isub() should be (3)    
+  }
+
+  // imul
+
+  "Bytecode.imul" should "append an imul instruction" in {
+    val bc = Bytecode()
+    bc.imul()
+    bc.result(0) should be (Opcode.imul)
+  }
+
+   it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.imul()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.imul() should be (3)    
+  }
+
+  // iconst
+
+  "Bytecode.iconst" should "append an iconst instruction" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.result(0) should be (Opcode.iconst)
+  }
+
+  it should "append the given constant" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.result(1) should be (42)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.adress should be (2)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.iconst(42) should be (3)    
+  }
+
+  // goto
+
+  "Bytecode.goto" should "append a goto instruction" in {
+    val bc = Bytecode()
+    bc.goto(0)
+    bc.result(0) should be (Opcode.goto)
+  }
+
+  it should "append an offset computed from the given target" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.goto(5)
+    bc.result(3) should be (3)
+
+    bc.iconst(27)
+    bc.goto(0)
+    bc.result(7) should be (-6)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.goto(0)
+    bc.adress should be (2)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.goto(100) should be (3)    
+  }
+
+  // ifeq
+
+  "Bytecode.ifeq" should "append an ifeq instruction" in {
+    val bc = Bytecode()
+    bc.ifeq(0)
+    bc.result(0) should be (Opcode.ifeq)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.ifeq(0)
+    bc.adress should be (2)
+  }
+
+  it should "append an offset computed from the given target" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.ifeq(5)
+    bc.result(3) should be (3)
+
+    bc.iconst(27)
+    bc.ifeq(0)
+    bc.result(7) should be (-6)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.ifeq(100) should be (3)    
+  }
+
+  // ifne
+
+  "Bytecode.ifne" should "append an ifne instruction" in {
+    val bc = Bytecode()
+    bc.ifne(0)
+    bc.result(0) should be (Opcode.ifne)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.ifne(0)
+    bc.adress should be (2)
+  }
+
+  it should "append an offset computed from the given target" in {
+    val bc = Bytecode()
+    bc.iconst(42)
+    bc.ifne(5)
+    bc.result(3) should be (3)
+
+    bc.iconst(27)
+    bc.ifne(0)
+    bc.result(7) should be (-6)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.ifne(100) should be (3)    
+  }
+
+  // print
+
+  "Bytecode.print" should "append a print instruction" in {
+    val bc = Bytecode()
+    bc.print()
+    bc.result(0) should be (Opcode.print)
+  }
+
+  it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.print()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.print() should be (3)    
+  }
+
+  // iload
+
+  "Bytecode.iload" should "append an iload instruction" in {
+    val bc = Bytecode()
+    bc.iload(42)
+    bc.result(0) should be (Opcode.iload)
+  }
+
+  it should "append the given offset" in {
+    val bc = Bytecode()
+    bc.iload(42)
+    bc.result(1) should be (42)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.iload(42)
+    bc.adress should be (2)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.iload(100) should be (3)    
+  }
+
+  // istore
+
+
+  "Bytecode.istore" should "append an istore instruction" in {
+    val bc = Bytecode()
+    bc.istore(42)
+    bc.result(0) should be (Opcode.istore)
+  }
+
+  it should "append the given offset" in {
+    val bc = Bytecode()
+    bc.istore(42)
+    bc.result(1) should be (42)
+  }
+
+  it should "use two slots in the bytecode" in {
+    val bc = Bytecode()
+    bc.istore(42)
+    bc.adress should be (2)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.istore(100) should be (3)    
+  }
+
+
+  // exit
+
+  "Bytecode.exit" should "append am exit instruction" in {
+    val bc = Bytecode()
+    bc.exit()
+    bc.result(0) should be (Opcode.exit)
+  }
+
+  it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.exit()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.exit() should be (3)    
+  }
+
+  // pop
+
+  "Bytecode.pop" should "append a pop instruction" in {
+    val bc = Bytecode()
+    bc.pop()
+    bc.result(0) should be (Opcode.pop)
+  }
+
+  it should "use one slot in the bytecode" in {
+    val bc = Bytecode()
+    bc.pop()
+    bc.adress should be (1)
+  }
+
+  it should "return the adress of the appended instruction" in {
+    val bc = Bytecode()
+    bc.append(1)
+    bc.append(2)
+    bc.append(3)
+    bc.pop() should be (3)    
+  }
+}
